41428755656b2efc2452bf436b785ce760f0fca9
Tomasz Kopacki <tomasz@kopacki.net>
2021-08-19 18:34:49 +1000

    try tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import 'nextra-theme-blog/style.css'
+// import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
 
-import '../styles/main.css'
+import 'tailwindcss/tailwind.css'
+// import '../styles/main.css'
 
 export default function Nextra({ Component, pageProps }) {
   return (
