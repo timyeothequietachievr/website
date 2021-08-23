a744e949a08717edc472e1e1b8d9f310b79fc2a4
Tomasz Kopacki <tomasz@kopacki.net>
2021-08-23 18:31:53 +1000

    feat(landing-page): v1
    Going live like there is no tommorow

---
+++ components/Main.jsx
@@ -0,0 +1,5 @@
+export default function Main({ children }) {
+  return (
+    <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">{children}</main>
+  )
+}
---
+++ components/Shell.jsx
@@ -0,0 +1,3 @@
+export default function Shell({ children }) {
+  return <section className="max-w-7xl mx-auto px-4 sm:px-6">{children}</section>
+}
