af34234d95ab636c7a85ccd0cc8e84e6785467d6
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-09-08 22:16:48 +1000

    redo

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,5 @@
 import Masthead from '../components/Masthead'
-import Feature1 from '../components/Feature1'
-import Feature2 from '../components/Feature2'
+import Feature from '../components/Feature'
 import Pricing from '../components/Pricing'
 import WhatDoYouGet from '../components/WhatDoYouGet'
 import Testimonials from '../components/Testimonials'
