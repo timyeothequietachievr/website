7e8e07c1a7cbd8f6ce18b7f88e2ff0f59205030e
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-09-13 23:50:32 +1000

    more testimonials

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -22,7 +22,7 @@ export default function Testimonials() {
                 <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
               </svg>
               <p className="relative">
-              Tim went above and beyond! He didn't just help me get unstuck with all the surrounding problems I was facing, but he reminded me that I was taking the right steps. He shared some actionable tips, was a great listener, patient, observative, and clever. I highly recommend; our session was therapeutic!
+              Speaking to Tim was like talking to a supportive, knowledgeable, and empathetic friend, I immediately felt comfortable on our zoom call. He gave me incredible advice and methods I could use in real-life scenarios. Tim showed me how to build confidence to speak up in uncomfortable situations. I highly recommend.
               </p>
             </div>
             <footer className="mt-8">
@@ -36,10 +36,10 @@ export default function Testimonials() {
                 </div>
                 <div className="ml-4">
                   <div className="text-base font-medium text-white">
-                    Wilson Thai
+                    Ezra Ju
                   </div>
                   <div className="text-base font-medium text-indigo-200">
-                    UX/UI Designer
+                    UX Designer, 86 400
                   </div>
                 </div>
               </div>
@@ -64,7 +64,7 @@ export default function Testimonials() {
                 <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
               </svg>
               <p className="relative">
-              Tim's such a great listener! He asked me the right questions to understand my queries, and provided me with actionable points that I can start using right away. He shared resources with me that are relevant to my current challenges, can't wait to use them! Totally recommended!
+              Speaking to Tim was like talking to a supportive, knowledgeable, and empathetic friend, I immediately felt comfortable on our zoom call. He gave me incredible advice and methods I could use in real-life scenarios. Tim showed me how to build confidence to speak up in uncomfortable situations. I highly recommend.
               </p>
             </div>
             <footer className="mt-8">
@@ -109,7 +109,7 @@ export default function Testimonials() {
                 <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
               </svg>
               <p className="relative">
-              Tim went above and beyond! He didn't just help me get unstuck with all the surrounding problems I was facing, but he reminded me that I was taking the right steps. He shared some actionable tips, was a great listener, patient, observative, and clever. I highly recommend; our session was therapeutic!
+              Tim went above and beyond. He didn't just help me get unstuck with all the surrounding problems I was facing, but he reminded me that I was taking the right steps. He shared some actionable tips, was a great listener, patient, observative, and clever. I highly recommend; our session was therapeutic!
               </p>
             </div>
             <footer className="mt-8">
@@ -151,7 +151,7 @@ export default function Testimonials() {
                 <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
               </svg>
               <p className="relative">
-              Tim's such a great listener! He asked me the right questions to understand my queries, and provided me with actionable points that I can start using right away. He shared resources with me that are relevant to my current challenges, can't wait to use them! Totally recommended!
+              Tim gave me valuable feedback and insights that I haven't even though of. The way he shared strategies and experience positioned me better on the market and get my first job. Thank you!
               </p>
             </div>
             <footer className="mt-8">
@@ -165,10 +165,97 @@ export default function Testimonials() {
                 </div>
                 <div className="ml-4">
                   <div className="text-base font-medium text-white">
-                    Leonardo Mattei
+                    Sophie Matrai
                   </div>
                   <div className="text-base font-medium text-indigo-200">
-                    Principal Product Designer, Financial Times
+                    Product Designer, Hipages
+                  </div>
+                </div>
+              </div>
+            </footer>
+          </blockquote>
+        </div>
+      </div>
+      <div className="max-w-7xl mx-auto md:grid md:grid-cols-2 md:px-6 lg:px-8">
+        <div className="py-12 px-4 sm:px-6 md:flex md:flex-col md:py-16 md:pl-0 md:pr-10 md:border-r md:border-indigo-900 lg:pr-16">
+          <div className="md:flex-shrink-0">
+            <img
+              className="h-12"
+              src="https://tailwindui.com/img/logos/tuple-logo-indigo-300.svg"
+              alt="Tuple"
+            />
+          </div>
+          <blockquote className="mt-6 md:flex-grow md:flex md:flex-col">
+            <div className="relative text-lg font-medium text-white md:flex-grow">
+              <svg
+                className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-indigo-600"
+                fill="currentColor"
+                viewBox="0 0 32 32"
+                aria-hidden="true"
+              >
+                <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+              </svg>
+              <p className="relative">
+                Tim is great; we covered every query I got and I left having closure, rare find! His experience resonated with mine, having gone through what I am currently going through. 
+              </p>
+            </div>
+            <footer className="mt-8">
+              <div className="flex items-start">
+                <div className="flex-shrink-0 inline-flex rounded-full border-2 border-white">
+                  <img
+                    className="h-12 w-12 rounded-full"
+                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </div>
+                <div className="ml-4">
+                  <div className="text-base font-medium text-white">
+                    Lewis Kang'ethe Ngugi
+                  </div>
+                  <div className="text-base font-medium text-indigo-200">
+                    Product Designer, Lookback.io
+                  </div>
+                </div>
+              </div>
+            </footer>
+          </blockquote>
+        </div>
+        <div className="py-12 px-4 border-t-2 border-indigo-900 sm:px-6 md:py-16 md:pr-0 md:pl-10 md:border-t-0 md:border-l lg:pl-16">
+          <div className="md:flex-shrink-0">
+            <img
+              className="h-12"
+              src="https://tailwindui.com/img/logos/workcation-logo-indigo-300.svg"
+              alt="Workcation"
+            />
+          </div>
+          <blockquote className="mt-6 md:flex-grow md:flex md:flex-col">
+            <div className="relative text-lg font-medium text-white md:flex-grow">
+              <svg
+                className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-indigo-600"
+                fill="currentColor"
+                viewBox="0 0 32 32"
+              >
+                <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+              </svg>
+              <p className="relative">
+              I highly recommend Tim. He provided tactics and strategies I can use as an introverted designer. He listened very attentively and asked me to describe work scenarios so that he could give advice appropriate to them. He also provided some handy questions that I can use for my 1:1 conversations and meetings.
+              </p>
+            </div>
+            <footer className="mt-8">
+              <div className="flex items-start">
+                <div className="flex-shrink-0 inline-flex rounded-full border-2 border-white">
+                  <img
+                    className="h-12 w-12 rounded-full"
+                    src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </div>
+                <div className="ml-4">
+                  <div className="text-base font-medium text-white">
+                    Andrew Cuevas
+                  </div>
+                  <div className="text-base font-medium text-indigo-200">
+                    UX Designer
                   </div>
                 </div>
               </div>
