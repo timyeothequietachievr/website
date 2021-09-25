2747e7565bd5440ed222607712b984d441076848
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-09-26 09:02:02 +1000

    tagline life hacks

--- components/Masthead.jsx
+++ components/Masthead.jsx
@@ -3,14 +3,17 @@ export default function Masthead() {
     <section className="py-16 bg-white overflow-hidden lg:py-24">
       <div className="text-center">
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-          <span className="block xl:inline">the</span>{' '}
+          <span className="block xl:inline">life hacks for</span>{' '}
           <span className="block text-indigo-600 xl:inline">
-            quiet
+            introverts
           </span>
-          <span className="block xl:inline"> achiever</span>{' '}
+          <span className="block xl:inline"> to have</span>{' '}
+          <span className="block text-indigo-600 xl:inline">
+            impact at work
+          </span>
         </h1>
         <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-        Helping introverts have impact and influence without pretending to be extroverts.
+        without pretending to be extroverted.
         </p>
         <div className="mt-5 px-4 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
           <div className="rounded-md shadow">
