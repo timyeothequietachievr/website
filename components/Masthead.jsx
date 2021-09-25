4528a00382022210d28fe7a2ec8231d2c9617311
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-09-26 09:33:06 +1000

    fiddle with masthead

--- components/Masthead.jsx
+++ components/Masthead.jsx
@@ -4,13 +4,9 @@ export default function Masthead() {
       <div className="text-center">
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
           <span className="block xl:inline">life hacks for</span>{' '}
-          <span className="block text-indigo-600 xl:inline">
-            introverts
-          </span>
-          <span className="block xl:inline"> to have</span>{' '}
-          <span className="block text-indigo-600 xl:inline">
-            impact at work
-          </span>
+          <span className="block text-indigo-600 xl:inline"> introverts</span>
+          <span className="block xl:inline">to have</span>{' '}
+          <span className="block text-indigo-600 xl:inline">impact at work</span>
         </h1>
         <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
         without pretending to be extroverted.
