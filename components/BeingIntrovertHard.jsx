5c6a7ce36003d79d24c00638c491f14c77308d41
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-09-26 18:03:57 +1000

    meme image

--- components/BeingIntrovertHard.jsx
+++ components/BeingIntrovertHard.jsx
@@ -1,3 +1,4 @@
+import MoneyPay from './images/TQA-meme-moneypay.png';
 /*
   This example requires Tailwind CSS v2.0+ 
   
@@ -21,7 +22,7 @@ export default function BeingIntrovertHard() {
           <div className="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
             <img
               className="h-56 w-full object-cover lg:absolute lg:h-full"
-              src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
+              src="./images/TQA-meme-moneypay.png"
               alt=""
             />
           </div>
--- components/Masthead.jsx
+++ components/Masthead.jsx
@@ -9,7 +9,7 @@ export default function Masthead() {
           <span className="block text-indigo-600 xl:inline">impact at work</span>
         </h1>
         <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-        without pretending to be extroverted.
+        without pretending to be extroverts.
         </p>
         <div className="mt-5 px-4 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
           <div className="rounded-md shadow">
