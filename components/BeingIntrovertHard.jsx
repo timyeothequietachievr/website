4fc0516935355e998a6c1e27076f6b14b4e068ca
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-09-26 18:04:58 +1000

    remove meme image

--- components/BeingIntrovertHard.jsx
+++ components/BeingIntrovertHard.jsx
@@ -22,7 +22,7 @@ export default function BeingIntrovertHard() {
           <div className="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
             <img
               className="h-56 w-full object-cover lg:absolute lg:h-full"
-              src="./images/TQA-meme-moneypay.png"
+              src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
               alt=""
             />
           </div>
