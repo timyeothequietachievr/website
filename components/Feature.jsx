7948b706c518a17f0da261a903c6108309fc6620
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-09-26 18:49:58 +1000

    content(what you will learn)

--- components/Feature.jsx
+++ components/Feature.jsx
@@ -59,43 +59,23 @@ export default function Feature() {
   return (
     <div className="py-16 bg-gray-50 overflow-hidden lg:py-24">
       <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
-        <div className="relative">
-          <h2 className="text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          How it works
-          </h2>
-          <p className="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">
-          We offer video on-demand, weekly office hours, monthly guest speakers and an online community of introverts. 
-          </p>
-        </div>
-
         <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
           <div className="relative">
-            <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
-            Topics include:
-            </h3>
+            <h2 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+            What you will learn:
+            </h2>
             <p className="mt-3 text-lg text-gray-500">
-            How to speak up and perform in meetings, being more visible in your organisation, how to network online and in real-life, make small talk, getting better at public speaking, how to say no politely etc
+            <ul role="list">
+                  <li>How to speak up and perform in meetings</li>
+                  <li>Be more visible in your organisation</li>
+                  <li>Networking - online and in real life</li>
+                  <li>Small talk with people you just met</li>
+                  <li>Public speaking with confidence</li>
+                  <li>How to say no politely</li>
+                  <li>Be an introverted leader with impact</li>
+                </ul>
             </p>
-
-            <dl className="mt-10 space-y-10">
-              {transferFeatures.map((item) => (
-                <div key={item.id} className="relative">
-                  <dt>
-                    <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                      <item.icon className="h-6 w-6" aria-hidden="true" />
-                    </div>
-                    <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                      {item.name}
-                    </p>
-                  </dt>
-                  <dd className="mt-2 ml-16 text-base text-gray-500">
-                    {item.description}
-                  </dd>
-                </div>
-              ))}
-            </dl>
           </div>
-
           <div className="mt-10 -mx-4 relative lg:mt-0" aria-hidden="true">
             <img
               className="relative mx-auto"
