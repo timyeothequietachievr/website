599455af4338b89fb4ebbee3bba46e2b1a93da54
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-09-29 09:42:11 +1000

    linebreak

--- components/Feature.jsx
+++ components/Feature.jsx
@@ -77,7 +77,9 @@ export default function Feature() {
                   <li>Create an inclusive team for all, introverts included</li>
             </ul>
             
-            Techniques are organised around <strong>The Introvert Skill Tree</strong>. Go deep into Networking or go broad across all skills. Level up your way.
+            Techniques are organised around <strong>The Introvert Skill Tree</strong>. Go deep into Networking or go broad across all skills.
+            
+            Level up your way.
             </p>
           </div>
           <div className="mt-10 -mx-4 relative lg:mt-0" aria-hidden="true">
