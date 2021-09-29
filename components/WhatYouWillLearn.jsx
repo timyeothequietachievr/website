1a05eaa84e6272cd6067d91a6a53787ff6f68ff6
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-09-29 23:08:26 +1000

    what you will learn 1

--- components/WhatYouWillLearn.jsx
+++ components/WhatYouWillLearn.jsx
@@ -1,45 +1,189 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { VideoCameraIcon, CalendarIcon, UserCircleIcon, ClockIcon } from '@heroicons/react/outline'
 
-export default function Example() {
+import {
+
+  AnnotationIcon,
+  
+  ClockIcon,
+  
+  VideoCameraIcon,
+  
+  UserCircleIcon,
+  
+  MailIcon,
+  
+  CalendarIcon
+  
+  } from '@heroicons/react/outline'
+  
+    
+  
+  const transferFeatures = [
+  
+  {
+  
+  id: 1,
+  
+  name: 'Over 60+ techniques',
+  
+  description:
+  
+  'Videos are bite-sized, practical and easy to apply. Watch on-demand with new techniques dropped over time.',
+  
+  icon: VideoCameraIcon
+  
+  },
+  
+  {
+  
+  id: 2,
+  
+  name: 'Weekly office hours',
+  
+  description:
+  
+  'Walk in with a question or dial in to listen. Participate as much or as little as you want (embers-only via video conference).',
+  
+  icon: CalendarIcon
+  
+  },
+  
+  {
+  
+  id: 3,
+  
+  name: 'Monthly guest speakers',
+  
+  description:
+  
+  'Every month will feature a guest speaker (members-only via video conference).',
+  
+  icon: UserCircleIcon
+  
+  },
+  
+  {
+  
+  id: 4,
+  
+  name: '24/7 online community of introverts',
+  
+  description:
+  
+  'You are not alone. Share with fellow introverts for learning and support.',
+  
+  icon: ClockIcon
+  
+  }
+  
+  ]
+  
+  const communicationFeatures = [
+  
+  {
+  
+  id: 1,
+  
+  name: 'Mobile notifications',
+  
+  description:
+  
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+  
+  icon: AnnotationIcon
+  
+  },
+  
+  {
+  
+  id: 2,
+  
+  name: 'Reminder emails',
+  
+  description:
+  
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+  
+  icon: MailIcon
+  
+  }
+  
+  ]
+  
+    
+  
+  export default function WhatYouWillLearn() {
+  
   return (
-    <div className="relative bg-white pt-16 pb-32 overflow-hidden">
-      <div className="relative">
-        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-2 lg:max-w-none lg:mx-0 lg:px-0">
-            <div>
-              <div className="mt-6">
-                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
-                  What you will learn: 
-                </h2>
-                <p className="mt-5 prose prose-indigo text-gray-500">
-                <ul role="list">
-                  <li>How to speak up and perform in meetings</li>
-                  <li>Be more visible in your organisation</li>
-                  <li>Networking - online and in real life</li>
-                  <li>Small talk - with people you just met</li>
-                  <li>Public speaking - prepare and perform with confidence</li>
-                  <li>How to say no politely</li>
-                  <li>Facilitate workshops</li>
-                  <li>Be an introverted leader with impact</li>
-                  <li>Create an inclusive team for all, introverts included</li>
-                </ul>
-                Techniques are organised around <strong>The Introvert Skill Tree</strong>. Go deep into Networking or go broad across all skills. You choose what you learn, when you need to learn it.<br /><br /><strong>Level up</strong> your way.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="mt-12 sm:mt-16 lg:mt-0">
-            <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-              <img
-                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg"
-                alt="Inbox user interface"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+  
+  <div className="py-2 bg-gray-50 overflow-hidden lg:py-2">
+  
+  <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
+  
+  <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+  
+  <div className="relative">
+  
+  <h2 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+  
+  What you will learn:
+  
+  </h2>
+  
+  <p className="mt-5 prose prose-indigo text-gray-500">
+  
+  <ul role="list">
+  
+  <li>How to speak up and perform in meetings</li>
+  
+  <li>Be more visible in your organisation</li>
+  
+  <li>Networking - online and in real life</li>
+  
+  <li>Small talk - with people you just met</li>
+  
+  <li>Public speaking - prepare and perform with confidence</li>
+  
+  <li>How to say no politely</li>
+  
+  <li>Facilitate workshops</li>
+  
+  <li>Be an introverted leader with impact</li>
+  
+  <li>Create an inclusive team for all, introverts included</li>
+  
+  </ul>
+  
+  Techniques are organised around <strong>The Introvert Skill Tree</strong>. Go deep into Networking or go broad across all skills. You choose what you learn, when you need to learn it.<br /><br /><strong>Level up</strong> your way.
+  
+  </p>
+  
+  </div>
+  
+  <div className="mt-10 -mx-4 relative lg:mt-0" aria-hidden="true">
+  
+  <img
+  
+  className="relative mx-auto"
+  
+  width={490}
+  
+  src="..images/always-be-networking.png"
+  
+  alt=""
+  
+  />
+  
+  </div>
+  
+  </div>
+  
+    
+  
+  </div>
+  
+  </div>
+  
   )
-}
+  
+  }
