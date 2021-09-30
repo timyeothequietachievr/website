d143b7b8c78045d2e0c51459c4c6604bcfdb63e0
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-09-30 23:04:51 +1000

    require

--- components/BeingIntrovertHard.jsx
+++ components/BeingIntrovertHard.jsx
@@ -1,6 +1,6 @@
 /*
-import ximage from './src/moneypay.png';
-
+import ximage from '../src/moneypay.png';
+src/moneypay.png
 This example requires Tailwind CSS v2.0+ 
   
   This example requires some changes to your config:
@@ -23,7 +23,7 @@ export default function BeingIntrovertHard() {
           <div className="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
             <img
               className="h-56 w-full object-cover lg:absolute lg:h-full"
-              src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
+              src={require('./images/moneypay.png').default}
               alt=""
             />
           </div>
