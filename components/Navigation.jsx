f8c647113c3235074aa64b0cf752f381ae1e12b3
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-10-01 10:08:40 +1000

    classname

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -100,6 +100,7 @@ function Logo() {
         { <a href="#">
           <span className="sr-only">Workflow</span>
           <img
+            className="h-8 w-auto sm:h-10"
             src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
             alt=""
           />
