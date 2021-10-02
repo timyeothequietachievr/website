3fa92a02729a0b17e062985b010aca0a03e6811d
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-10-02 22:04:16 +1000

    community

--- components/Feature2.jsx
+++ components/Feature2.jsx
@@ -77,7 +77,8 @@ export default function Example() {
             <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg"
+                /* src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg" */
+                src="src/TQA-community.png"
                 alt="Customer profile user interface"
               />
             </div>
