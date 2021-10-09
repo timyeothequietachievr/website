571f8247028e375b26aee08df8dde7256f60d589
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-10-09 21:08:55 +1100

    currencies

--- components/payment-options/index.jsx
+++ components/payment-options/index.jsx
@@ -8,27 +8,83 @@ export function usePaymentOptions() {
         stripeLink: 'https://stripe.com/aud/monthly',
         price: 19.99,
         currencyName: 'AUD',
-        periodName: 'mth',
+        periodName: 'mo',
       },
       yearly: {
         stripeLink: 'https://stripe.com/aud/yearly',
-        price: 199.99,
+        price: 15.99,
         currencyName: 'AUD',
-        periodName: 'yr',
+        periodName: 'mo',
+      }
+    },
+    usd: {
+      monthly: {
+        stripeLink: 'https://stripe.com/usd/monthly',
+        price: 14.99,
+        currencyName: 'USD',
+        periodName: 'mo',
+      },
+      yearly: {
+        stripeLink: 'https://stripe.com/usd/yearly',
+        price: 11.99,
+        currencyName: 'USD',
+        periodName: 'mo',
       }
     },
     cad: {
       monthly: {
         stripeLink: 'https://stripe.com/cad/monthly',
-        price: 14.99,
+        price: 18.99,
         currencyName: 'CAD',
-        periodName: 'mth',
+        periodName: 'mo',
       },
       yearly: {
         stripeLink: 'https://stripe.com/cad/yearly',
-        price: 149.99,
+        price: 14.99,
         currencyName: 'CAD',
-        periodName: 'yr',
+        periodName: 'mo',
+      }
+    },
+    gbp: {
+      monthly: {
+        stripeLink: 'https://stripe.com/gbp/monthly',
+        price: 9.99,
+        currencyName: 'GBP',
+        periodName: 'mo',
+      },
+      yearly: {
+        stripeLink: 'https://stripe.com/gbp/yearly',
+        price: 7.99,
+        currencyName: 'GBP',
+        periodName: 'mo',
+      }
+    },
+    eur: {
+      monthly: {
+        stripeLink: 'https://stripe.com/eur/monthly',
+        price: 12.99,
+        currencyName: 'EUR',
+        periodName: 'mo',
+      },
+      yearly: {
+        stripeLink: 'https://stripe.com/eur/yearly',
+        price: 10.99,
+        currencyName: 'EUR',
+        periodName: 'mo',
+      }
+    },
+    sgd: {
+      monthly: {
+        stripeLink: 'https://stripe.com/sgd/monthly',
+        price: 19.88,
+        currencyName: 'SGD',
+        periodName: 'mo',
+      },
+      yearly: {
+        stripeLink: 'https://stripe.com/sgd/yearly',
+        price: 15.88,
+        currencyName: 'SGD',
+        periodName: 'mo',
       }
     }
   }
