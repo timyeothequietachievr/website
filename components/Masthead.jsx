89232da982fff81fe08cfcb21dd112fb1471a6f0
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-10-09 22:24:43 +1100

    youtube

--- components/Masthead.jsx
+++ components/Masthead.jsx
@@ -17,7 +17,7 @@ export default function Masthead() {
               href="#"
               className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
             >
-              Buy Now $19.99/mo
+              Buy Now
             </a>
           </div>
           <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
