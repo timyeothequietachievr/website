c4bcd23065791873145b8791177b6a4aba697eb8
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-10-09 22:26:24 +1100

    youtube

--- components/Feature1.jsx
+++ components/Feature1.jsx
@@ -51,7 +51,7 @@ export default function Example() {
             <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://youtu.be/QRn5SFM97pA"
+                src="https://www.youtube.com/watch?v=QRn5SFM97pA"
                 /* src="src/demoVideo.gif" */
                 alt="Inbox user interface"
               />
