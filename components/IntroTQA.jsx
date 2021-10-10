57554b1e7092d492c85c56385516b32891c2aec2
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-10-10 22:36:50 +1100

    link

--- components/IntroTQA.jsx
+++ components/IntroTQA.jsx
@@ -1,7 +1,7 @@
 /* This example requires Tailwind CSS v2.0+ */
 export default function Example() {
     return (
-      <div className="relative bg-indigo-700">
+      <div className="relative bg-indigo-300">
         <div className="absolute inset-0">
           <img
             className="w-full h-full object-cover"
@@ -14,7 +14,7 @@ export default function Example() {
         <h2 class="text-white font-semibold text-indigo-600 tracking-wide">introducing</h2>
           <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">the quiet achiever</h1>
           <p className="mt-6 text-xl text-indigo-100 max-w-3xl">
-          Tim Yeo has spent his career in rooms full of strong opinions and loud voices. A designer and leader in startups for 16 years, he's spoken at international conferences and has coached nearly a hundred introverts. 
+          Tim Yeo has spent his career in rooms full of strong opinions and loud voices. A designer and leader in startups for 16 years, he's spoken at international conferences and has coached nearly a hundred introverts. <a href="/about"> Learn more about Tim</a> 
           </p>
         </div>
       </div>
