e97d96954e7d1b54787426fcaaca51d92860132f
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-10-10 17:55:35 +1100

    indigo 850 950

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,19 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                'tqa-blue-dark': '#022351',
-                'tqa-blue-darker': '#011632',
-                'tqa-blue-light': '#1441FA',
-                'tqa-blue-lighter': '#103AE5',
-                'tqa-red': '#FA2A37',
-                'tqa-red-darker': '#E52531',
-                'tqa-yellow': '#FFB700',
-                'tqa-beige': '#FEF8F1',
-                'regal-grey': '#EBEBEB',
+                indigo: {
+                    850: '#022351',
+                    950: '#011632'
+                }
+                // 'tqa-blue-dark': '#022351',
+                // 'tqa-blue-darker': '#011632',
+                // 'tqa-blue-light': '#1441FA',
+                // 'tqa-blue-lighter': '#103AE5',
+                // 'tqa-red': '#FA2A37',
+                // 'tqa-red-darker': '#E52531',
+                // 'tqa-yellow': '#FFB700',
+                // 'tqa-beige': '#FEF8F1',
+                // 'regal-grey': '#EBEBEB',
             }
         },
     },
