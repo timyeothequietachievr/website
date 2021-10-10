b8a2784a93e8f616d8bebbb97e622769359178ac
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-10-10 17:43:49 +1100

    extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
     purge: ['./pages/**/*.{js,ts,jsx,tsx,mdx,md}', './components/**/*.{js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'tqa-blue-dark': '#022351',
+                'tqa-blue-darker': '#011632',
+                'tqa-blue-light': '#1441FA',
+                'tqa-blue-lighter': '#103AE5',
+                'tqa-red': '#FA2A37',
+                'tqa-red-darker': '#E52531',
+                'tqa-yellow': '#FFB700',
+                'tqa-beige': '#FEF8F1',
+                'regal-grey': '#EBEBEB',
+            }
+        },
     },
     variants: {
         extend: {},
