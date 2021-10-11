4a114e633c793769fe8515307750deb5ec015fa6
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-10-11 23:53:24 +1100

    toaster

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Toaster from '../components/Toaster'
 import Masthead from '../components/Masthead'
 import BeingIntrovertHard from '../components/BeingIntrovertHard'
 import WhatYouWillLearn from '../components/WhatYouWillLearn'
@@ -14,6 +15,7 @@ import IntroTQA from '../components/IntroTQA'
 export default function IndexPage() {
   return (
     <>
+      <Toaster />
       <Masthead />
       <BeingIntrovertHard />
       <IntroTQA />
