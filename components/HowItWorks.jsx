b5d94a823c39a5f9995184c65074f41f8a1dec27
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-11-08 23:06:26 +1100

    icons

--- components/HowItWorks.jsx
+++ components/HowItWorks.jsx
@@ -1,5 +1,8 @@
 /* This example requires Tailwind CSS v2.0+ */
 import {
+    PlayIcon,
+    UserGroupIcon,
+    ShareIcon,
     CloudUploadIcon,
     CogIcon,
     LockClosedIcon,
@@ -9,9 +12,9 @@ import {
   } from '@heroicons/react/outline'
   
   const features = [
-    { name: 'Watch', icon: CloudUploadIcon, para: 'Videos are on-demand. Learn at your own pace, binge watch if you like. Watch them on repeat. You decide how best you learn.' },
-    { name: 'Practice', icon: LockClosedIcon, para: 'Practice at work, practice on your own, or with your loved ones. Find a real life situation where you can practice for real, something just a little outside of your comfort zone.' },
-    { name: 'Share', icon: RefreshIcon, para: 'Share what you made or how things went, with people just like you. People who get it. That is what we the community are here for. We will listen, troubleshoot, offer feedback, celebrate your wins and support you when you try again.' },
+    { name: 'Watch', icon: PlayIcon, para: 'Videos are on-demand. Learn at your own pace, binge watch if you like. Watch them on repeat. You decide how best you learn.' },
+    { name: 'Practice', icon: UserGroupIcon, para: 'Practice at work, practice on your own, or with your loved ones. Find a real life situation where you can practice for real, something just a little outside of your comfort zone.' },
+    { name: 'Share', icon: ShareIcon, para: 'Share what you made or how things went, with people just like you. People who get it. That is what we the community are here for. We will listen, troubleshoot, offer feedback, celebrate your wins and support you when you try again.' },
   ]
   
   export default function Example() {
