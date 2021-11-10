3e1cef11c3901269a651c8a94df489828c1e3954
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-11-10 22:35:57 +1100

    tweaks

--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -20,7 +20,7 @@ export default function Newsletter() {
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center">
           <div className="lg:w-0 lg:flex-1">
             <h2 className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl" id="newsletter-headline">
-              Sign up for our newsletter
+              Subscribe to our newsletter
             </h2>
             <p className="mt-3 max-w-3xl text-lg leading-6 text-gray-300">
               {/* shortly. */}
@@ -45,16 +45,17 @@ export default function Newsletter() {
                   type="submit"
                   className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
                 >
-                  Notify me
+                  Coming soon 🤫
                 </button>
               </div>
             </form>
-            <p className="mt-3 text-sm text-gray-300">
+            {/*<p className="mt-3 text-sm text-gray-300">
               We care about the protection of your data. Read our{' '}
               <a href="#" className="text-white font-medium underline">
                 Privacy Policy.
               </a>
             </p>
+            */}
           </div>
         </div>
       </div>
