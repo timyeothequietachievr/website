8e888caf971c38cacdcdffb5b07137d81009ba98
Tomasz Kopacki <tomasz@kopacki.net>
2021-11-11 09:30:46 +1100

    feat(modal): intro

---
+++ components/modal/index.jsx
@@ -0,0 +1,96 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+
+export function useModal() {
+  const [isOpened, setIsOpened] = useState(false)
+
+  const open = () => setIsOpened(true)
+  const close = () => setIsOpened(false)
+
+  return {
+    isOpened,
+    open,
+    close
+  }
+}
+
+export function Modal({ children, close, isOpened }) {
+  return (
+    <Transition.Root show={isOpened} as={Fragment}>
+      <Dialog as="div" className="fixed z-10 inset-10" onClose={close}>
+        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div className="inline-block bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all align-middle w-full sm:max-w-sm lg:max-w-lg xl:max-w-xl">
+              {children}
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}
+
+export function IntroVideoModalContents({ ytLink }) {
+  return (
+    <>
+      <div>
+        <div className="mt-3 text-center sm:mt-5">
+          <Dialog.Title
+            as="h3"
+            className="text-lg leading-6 font-medium text-gray-900"
+          >
+            Intro
+          </Dialog.Title>
+          <div className="mt-2">
+            <iframe
+              className="max-w-full"
+              height="360"
+              width="640"
+              src={ytLink}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </div>
+        </div>
+      </div>
+      <div className="mt-5 sm:mt-6">
+        <button
+          type="button"
+          className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+          onClick={close}
+        >
+          Go back
+        </button>
+      </div>
+    </>
+  )
+}
