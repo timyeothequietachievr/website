aacd6f3b9836da73d983a1a2bc9cae5fdd14eba9
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-11-12 18:40:03 +1100

    gift2

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,7 @@ import TestimonialLargeAvatar1 from '../components/TestimonialLargeAvatar1'
 import TestimonialLargeAvatar2 from '../components/TestimonialLargeAvatar2'
 import IntroTQA from '../components/IntroTQA'
 import Gift from '../components/Gift'
-import Gift from '../components/Gift2'
+import Gift2 from '../components/Gift2'
 import HowItWorks from '../components/HowItWorks'
 import { PricingContextProvider } from '../components/pricing-utils/Context'
 
