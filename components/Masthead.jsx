2c7b68edbfe902d76177212d1d2ca75785ac77cc
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2021-11-18 00:08:42 +1100

    extrabold

--- components/Masthead.jsx
+++ components/Masthead.jsx
@@ -11,7 +11,7 @@ export default function Masthead() {
           <span className="block xl:inline">to have</span>{' '}
           <span className="block text-indigo-600 xl:inline">impact at work</span>
         </h1>
-        <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+        <p className="mt-3 max-w-md mx-auto text-base text-gray-800 font-bold sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
         without pretending to be extroverts.
         </p>
         <div className="mt-5 px-4 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
