5ab3f20126bbd9573a77350d4d36009d272a0437
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2022-03-06 10:16:20 +1100

    anchor link monthlyguestspeakers

--- components/MonthlyGuestSpeakers.jsx
+++ components/MonthlyGuestSpeakers.jsx
@@ -100,7 +100,7 @@ const people = [
   
   export default function Example() {
     return (
-      <div className="bg-white">
+      <div className="bg-white" id="monthlyguestspeakers">
         <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
           <div className="space-y-12">
             <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
