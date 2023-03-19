7b97ee5d5bf468162040129d5a841dafdfd7f7e1
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2023-03-19 13:36:30 +1100

    download PDF

--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -20,7 +20,7 @@ export default function Newsletter() {
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center">
           <div className="lg:w-0 lg:flex-1">
             <h2 className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl" id="newsletter-headline">
-              Slides from UX Australia 2022
+              Download PDF - 4-week introvert course curriculum
             </h2>
             <p className="mt-3 max-w-3xl text-lg leading-6 text-gray-300">
               {/* shortly. */}
@@ -28,6 +28,9 @@ export default function Newsletter() {
           </div>
           <div className="mt-8 lg:mt-0 lg:ml-8">
             {/*
+            Slides from UX Australia 2022
+            https://forms.gle/7bgpGZBvdjKvEDy37
+
             <form className="sm:flex">
               <label htmlFor="email-address" className="sr-only">
                 Email address
@@ -44,7 +47,7 @@ export default function Newsletter() {
               */}
               <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
                 <a
-                  href="https://forms.gle/DZBWcDmYSJ6brxPj9#"
+                  href="https://forms.gle/iz3yG32BFKrGxS4CA"
                   className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
                 >
                   Download Now
