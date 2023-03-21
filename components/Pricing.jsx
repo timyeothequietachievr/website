cd3a872b8d47a59c3fce928c6aacafd1b1cc3305
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2023-03-21 23:15:35 +1100

    "flex-shrink-0 w-6 h-6 text-indigo-500"

--- components/Pricing.jsx
+++ components/Pricing.jsx
@@ -90,7 +90,7 @@ export default function Pricing() {
                   ))}
                 </ul>
               </div>
-              <a href={idx === 0 ? stripeLinkPaymentPlanMonthly : tier.localisedstripeLinkPaymentPlanMonthly(currencyName.toLowerCase())} className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+              <a href={idx === 0 ? stripeLinkPaymentPlanMonthly : tier.localisedstripeLinkPaymentPlanMonthly(currencyName.toLowerCase())} className="flex-shrink-0 w-6 h-6 text-indigo-500">
               {/* <a href={idx === 0 ? stripeLinkPaymentPlanMonthly : tier.localisedstripeLinkPaymentPlanMonthly(currencyName.toLowerCase())}> */}
                 split payment over 4 months
               </a>    
