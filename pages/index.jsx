862c9655e1b434a9a416547ab7ec50f8748fbd23
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2023-03-28 18:51:31 +1100

    google analytics

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,6 +23,15 @@ import { PricingContextProvider } from '../components/pricing-utils/Context'
 
 export default function IndexPage() {
   return (
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TVY6L0GW07"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TVY6L0GW07');
+</script>
     <PricingContextProvider>
       <Masthead />
       <BeingIntrovertHard />
