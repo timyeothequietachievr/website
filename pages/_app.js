19e8e87c822b5831f0da8189604c11c657852a02
Tomasz Kopacki <tomasz@kopacki.net>
2023-04-04 18:12:55 +1000

    feat: add ga4 tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
+import { useEffect } from 'react';
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+
+import * as gtag from '../lib/gtag'
 
 import Navigation from '../components/Navigation'
 import Footer from '../components/Footer'
@@ -14,6 +19,39 @@ const navigationItems = [
 const loginLink = 'https://circle.so'
 
 export default function Nextra({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+
+    const trackGaEvents = (e) => {
+      if (e.target.matches('[data-event-category][data-event-action][data-event-label]')) {
+        let eventData = {
+          action: e.target.dataset.eventAction,
+          category: e.target.dataset.eventCategory,
+          label: e.target.dataset.eventLabel,
+        };
+
+        if (e.target.dataset.eventValue) {
+          eventData.value = e.target.dataset.eventValue;
+        }
+
+        gtag.event(eventData)
+      }
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    document.addEventListener('click', trackGaEvents);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+      document.removeEventListener('click', trackGaEvents);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
@@ -28,6 +66,24 @@ export default function Nextra({ Component, pageProps }) {
           rel="stylesheet"
         />
       </Head>
+
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       
       {  <Toaster /> }
       <Navigation items={navigationItems} loginLink={loginLink} />
