49cbb183386e0fe8e3de2f1016316f30cb90f075
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2023-06-24 08:40:40 +1000

    remove icons

--- components/Video.jsx
+++ components/Video.jsx
@@ -14,7 +14,7 @@
 */
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+/*import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline' */
 
 const navigation = [
   { name: 'Product', href: '#' },
@@ -67,7 +67,7 @@ export default function Example() {
                 <div className="-mr-2 flex items-center md:hidden">
                   <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="sr-only">Open main menu</span>
-                    <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                    {/* <Bars3Icon className="h-6 w-6" aria-hidden="true" /> */}
                   </Popover.Button>
                 </div>
               </div>
@@ -116,7 +116,7 @@ export default function Example() {
                   <div className="-mr-2">
                     <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                       <span className="sr-only">Close main menu</span>
-                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                      {/* <XMarkIcon className="h-6 w-6" aria-hidden="true" /> */}
                     </Popover.Button>
                   </div>
                 </div>
