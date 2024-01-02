423aa487ff32f448cd0a3a47607fe5b541dde752
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-01-02 12:41:49 +1100

    book hero image

--- components/BookHero.jsx
+++ components/BookHero.jsx
@@ -33,7 +33,7 @@ export default function CourseLeadership() {
               <img
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
                 /* src="https://www.youtube.com/embed/QRn5SFM97pA" */
-                src="src/Course-Hero-Leadership.jpg"
+                src="src/bookhero.jpg"
                 alt="Leadership for introverts"
               />
             </div>
