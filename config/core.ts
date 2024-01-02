22f25e4030311cec4c773e4719972d649fbb8ce8
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-01-02 23:18:25 +1100

    book nav

--- config/core.ts
+++ config/core.ts
@@ -1,13 +1,14 @@
 export const navigationItems = [
   { name: 'Courses', href: '/courses' },
   { name: 'Coaching', href: '/coaching' },
+  { name: 'Book', href: '/book' },
   { name: 'About', href: '/about' }
 ]
 
 export const siteMetaData = {
   title: 'The Quiet Achiever',
   description:
-    'life hacks for introverts to have impact at work without pretending to be extroverts',
+    'tiny habits for quiet achievers to have impact at work without pretending to be extroverts',
   image: 'src/logo-tqa/svg'
 }
 
