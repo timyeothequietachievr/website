64bc0aaf9c96f31ede22a2c96c68b11cbffb4f4a
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-04-02 23:23:22 +1100

    book testimonials

--- pages/book.tsx
+++ pages/book.tsx
@@ -17,10 +17,13 @@ import Testimonials from '../components/Testimonials'
 import TestimonialBook2 from '../components/TestimonialBook2'
 import TestimonialInterview2 from '../components/TestimonialInterview2'
 import TestimonialInterview3 from '../components/TestimonialInterview3'
+import TestimonialInterview4 from '../components/TestimonialInterview4'
+import TestimonialInterview5 from '../components/TestimonialInterview5'
 import TestimonialBook1 from '../components/TestimonialBook1'
 import TestimonialLeadership2 from '../components/TestimonialLeadership2'
 import TestimonialLeadership3 from '../components/TestimonialLeadership3'
 import TestimonialLeadership4 from '../components/TestimonialLeadership4'
+import TestimonialLeadership5 from '../components/TestimonialLeadership5'
 import TestimonialLargeAvatar1 from '../components/TestimonialLargeAvatar1'
 import TestimonialLargeAvatar2 from '../components/TestimonialLargeAvatar2'
 import TestimonialLargeAvatar3 from '../components/TestimonialLargeAvatar3'
@@ -36,9 +39,12 @@ export default function CoursesPage() {
       <TestimonialLeadership2 />
       <TestimonialLeadership3 />
       <TestimonialLeadership4 />
+      <TestimonialLeadership5 />
       <TestimonialBook2 />
       <TestimonialInterview2 />
       <TestimonialInterview3 />
+      <TestimonialInterview4 />
+      <TestimonialInterview5 />
       <TestimonialLargeAvatar2 />
       <TestimonialLargeAvatar3 />
       <TestimonialLargeAvatar4 />
