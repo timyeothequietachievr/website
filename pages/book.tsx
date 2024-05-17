9d5839fa8f90029cd490d499219ec1702b9700db
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-05-17 23:03:45 +1000

    nup

--- pages/book.tsx
+++ pages/book.tsx
@@ -49,14 +49,14 @@ export default function CoursesPage() {
       <TestimonialLargeAvatar3 />
       <TestimonialLargeAvatar4 />
       <Testimonials />
-      <Pricing kind="course">
+      {/* <Pricing kind="course">
         <h2 className="text-3xl font-extrabold text-gray-900 sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">
           Start learning  
         </h2>
 
         <p className="mt-6 max-w-2xl text-xl text-gray-500">
         </p>
-      </Pricing>
+  </Pricing> */}
       <MastheadBook />
       {/* <Gift /> */}
       {/*<Gift2 /> */}
