48a840cfb200840618afe04e4f4f84774e893fa4
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-05-17 21:46:08 +1000

    watchvideos

--- components/Watchvideos.jsx
+++ components/Watchvideos.jsx
@@ -76,6 +76,78 @@ export default function Watchvideo1() {
           </div>
         </div>
       </div>
+      <div className="relative">
+        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+            <div id="leadership">
+              <div className="mt-6">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                   Quiet leadership for introverts (Mar 2024) - Design Buddies 
+                </h2>
+                <p className="mt-mt-5 prose prose-indigo text-gray-500 text-lg text-gray-500">
+                I speak to the Design Buddies community about how to lead as an introvert and have impact and influence even when you are working remotely<br /><br />
+                {/* Binge watch or practice as you go. Videos are bite-sized, easy to apply and leverage your introverted strengths. Learn the way that suits you. */}
+                </p>
+                <div className="mt-6">
+                  <a
+                    href="https://www.youtube.com/watch?v=BOOd2Q83EJA&t=2s"
+                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
+                  >
+                    Watch now 
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-12 sm:mt-16 lg:mt-0">
+            <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+              <img
+                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                /* src="https://www.youtube.com/embed/QRn5SFM97pA" */
+                src="src/video-designbuddies.jpeg"
+                alt="Video Design Buddies"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-24">
+        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+            <div id="interviews">
+              <div className="mt-6">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                  {/* Weekly office hours */}
+                   Quiet leadership for introverts (Nov 2023) - ADPList BeMore Festival
+                </h2>
+                <p className="mt-5 prose prose-indigo text-gray-500">
+                I speak at the ADPList BeMore Festival on quiet leadership<br />  
+                  {/* Once a week, we will meet via video conference. Ask a question or dial in to listen. No preparation required; participate as much or as little as you want. */}
+                </p>
+                <div className="mt-6">
+                  <a
+                    href="https://youtu.be/LdxDN2xOrEg"
+                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
+                  >
+                    Watch now
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
+            <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+              <img
+                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
+                /* src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg" */
+                /* src="https://www.sli.do/build/video/slido-polling-b82ad40.mp4" */
+                src="src/video-adplist.jpeg"
+                alt="ADPList BeMore Festival"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
