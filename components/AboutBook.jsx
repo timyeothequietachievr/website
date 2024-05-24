9ede8b272d8bf8cd725f8ef469a656000a81e893
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-05-24 23:32:47 +1000

    aboutbook

--- components/AboutBook.jsx
+++ components/AboutBook.jsx
@@ -11,21 +11,21 @@ export default function AboutBook() {
               Hi, I'm Tim Yeo
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-            I've spent my career as a designer and a leader in tech. I worked in rooms full of people with strong opinions and loud voices.<br></br>
-            For years, I struggled to manage my own introversion. I thought that if I wanted to be a leader, I needed to be an extrovert all the time. So I faked it, and it worked! But I was tired of pretending to be someone I was not. So I started doing things my own way.<br></br>
-            The Quiet Achiever is a collection of the tiny habits that helped me have impact and influence at work, while remaining my true authentic self. <br></br>
-            I developed, practiced and helped others learn these tiny habits over years of coaching and workshops. <br></br>
+            I've spent my career as a designer and a leader in tech. I worked in rooms full of people with strong opinions and loud voices.<br></br><br></br>
+            For years, I struggled to manage my own introversion. I thought that if I wanted to be a leader, I needed to be an extrovert all the time. So I faked it, and it worked! But I was tired of pretending to be someone I was not. So I started doing things my own way.<br></br><br></br>
+            The Quiet Achiever is a collection of the tiny habits that helped me have impact and influence at work, while remaining my true authentic self. <br></br><br></br>
+            I developed, practiced and helped others learn these tiny habits over years of coaching and workshops. <br></br><br></br>
             Now, I share these tiny habits with you in a book.
             </p>
             <div className="mt-10 flex items-center gap-x-6">
               <a
-                href="#"
+                href="#pricing"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
               </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
+              <a href="/about" className="text-sm font-semibold leading-6 text-gray-900">
+                More about Tim <span aria-hidden="true">→</span>
               </a>
             </div>
           </div>
@@ -33,7 +33,7 @@ export default function AboutBook() {
         <div className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
           <img
             className="aspect-[3/2] w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
-            src="https://images.unsplash.com/photo-1498758536662-35b82cd15e29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2102&q=80"
+            src="src/about-timyeo.jpg?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2102&q=80"
             alt=""
           />
         </div>
