0ab8407cc34b98b38a9f40e4dbde4fc6e77b65b5
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-06-10 12:26:25 +1000

    in form

--- components/ConvertkitNewsletter.jsx
+++ components/ConvertkitNewsletter.jsx
@@ -21,38 +21,12 @@ export default function ConvertkitNewsletter() {
         <div className="max-w-xl text-3xl font-bold tracking-tight text-white sm:text-4xl lg:col-span-7">
           <h2 className="inline sm:block lg:inline xl:block">Want product news and updates?</h2>{' '}
           <p className="inline sm:block lg:inline xl:block">Sign up for our newsletter.</p>
-        </div>
-        <aside ref={ckFormRef} />
-        {/*
+        </div>        
         <form className="w-full max-w-md lg:col-span-5 lg:pt-2">
           <div className="flex gap-x-4">
-            <label htmlFor="email-address" className="sr-only">
-              Email address
-            </label>
-            <input
-              id="email-address"
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-              placeholder="Enter your email"
-            />
-            <button
-              type="submit"
-              className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-            >
-              Subscribe
-            </button>
+            <aside ref={ckFormRef} />
           </div>
-          <p className="mt-4 text-sm leading-6 text-gray-300">
-            We care about your data. Read our{' '}
-            <a href="#" className="font-semibold text-white">
-              privacy&nbsp;policy
-            </a>
-            .
-          </p>
-        </form> */}
+        </form>
       </div>
     </div>
   )
