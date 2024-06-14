12981decdcad5f4d1a9d6452949dc83556bc13e3
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-06-14 16:08:19 +1000

    #tellyourfriends

--- components/SignupBookTellYourFriends.jsx
+++ components/SignupBookTellYourFriends.jsx
@@ -19,7 +19,7 @@ export default function SignupBookTellYourFriends() {
     <div className="bg-gray-900 py-16 sm:py-24 lg:py-32">
       <div className="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8">
         <div className="max-w-xl text-3xl font-bold tracking-tight text-white sm:text-4xl lg:col-span-7">
-          <h2 id="bookreferral"vclassName="inline sm:block lg:inline xl:block">Tell your friends</h2>{' '}
+          <h2 id="tellyourfriends"vclassName="inline sm:block lg:inline xl:block">Tell your friends</h2>{' '}
           <p className="mx-auto mt-2 max-w-xl text-left text-lg leading-8 text-gray-300">Email a friend who'd be interested in The Quiet Achiever book.</p> 
           {/* <p className="inline sm:block lg:inline xl:block">It's free.</p> */}
         </div>        
