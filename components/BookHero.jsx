95003fe09f9151f8e47cfe0354cb442352e5dc97
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-06-14 16:00:51 +1000

    bookhero

--- components/BookHero.jsx
+++ components/BookHero.jsx
@@ -1,7 +1,22 @@
 /* This example requires Tailwind CSS v2.0+ */
+import { useEffect, useRef } from 'react';
 import { VideoCameraIcon, CalendarIcon, UserCircleIcon, ClockIcon } from '@heroicons/react/outline'
 
 export default function BookHero() {
+  const ckFormRef = useRef();
+
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = 'https://thequietachievr.ck.page/a9395221a5/index.js';
+    script.async = true;
+    script.setAttribute('data-uid', 'a9395221a5');
+    ckFormRef.current.appendChild(script);
+
+    return () => {
+      ckFormRef.current.removeChild(script);
+    };
+  }, []);
   return (
     <div className="relative bg-white pt-2 pb-32 overflow-hidden">
       <div className="relative">
@@ -16,17 +31,7 @@ export default function BookHero() {
                 <br /><strong></strong><br />Life is too short to be living someone else's version of success.<br /><br />Stop pretending.<br /><br />Start practicing.<br /><br />Remain your true authentic self.<br /><br /><strong>Signup to: </strong><br />- Be the first to know when the book is launched<br />- Sneak peek of early chapters before they are published<br /><br />
                 {/* Binge watch or practice as you go. Videos are bite-sized, easy to apply and leverage your introverted strengths. Learn the way that suits you. */}
                 </p>
-                <div className="mt-6">
-                   <a
-                    href="https://forms.gle/VAN6n5r8qFhFEsKm7"
-                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
-                    data-event-category="email"
-                    data-event-action="click_signup_now"
-                    data-event-label="cta_book_hero"
-                  >
-                    Signup for book updates 
-                  </a>
-                </div>
+                <aside ref={ckFormRef} />
               </div>
             </div>
           </div>
