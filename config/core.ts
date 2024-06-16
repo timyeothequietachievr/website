dd49184d05e80e57431ea574d27ea1b46a47bd2c
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-06-17 00:01:55 +1000

    default USD

--- config/core.ts
+++ config/core.ts
@@ -14,7 +14,7 @@ export const siteMetaData = {
 }
 
 export const listingMetaData = {
-  currency: 'AUD',
+  currency: 'USD',
   frequency: 'yearly',
   get userLocale() {
     return typeof navigator !== 'undefined' ? navigator.language : 'en'
