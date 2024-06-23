f19a5208712301a730e04eab0dc5932b47002291
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-06-23 17:41:19 +1000

    author edition masthead

--- lib/offers/books.ts
+++ lib/offers/books.ts
@@ -346,15 +346,15 @@ const books: Array<BookOffer> = [
   },
   /*{ This includes the prices of everything digital after 9Aug
     name: 'Everything Digital',
-    badge: '9Aug',
+    badge: 'Download Now',
     features: ['📲 ePub, PDF','🎤 Audiobook narrated by the author','Join the Quiet Achiever community'],
     paymentOptions: {
       AUD: {
         currency: 'AUD',
-        price: 39.99,
+        price: 16.99,
         paymentLinks: {
-          split: '#NA',
-          full: '#NA'
+          split: 'https://buy.stripe.com/5kA8Aa3CzaTZ4JG02b',
+          full: 'https://buy.stripe.com/5kAeYy8WT1jp8ZW6r7'
         }
       },
 
@@ -362,59 +362,59 @@ const books: Array<BookOffer> = [
         currency: 'CAD',
         price: 35.88,
         paymentLinks: {
-          split: '#NA',
-          full: '#NA'
+          split: 'https://buy.stripe.com/bIYg2Cdd93rx8ZW4is',
+          full: '#https://buy.stripe.com/7sI8Aaa0Xfafekg6r8'
         }
       },
 
       EUR: {
         currency: 'EUR',
-        price: 23.88,
+        price: 9.99,
         paymentLinks: {
-          split: '#NA',
-          full: '#NA'
+          split: 'https://buy.stripe.com/3cs3fQ7SPe6ba4002e',
+          full: 'https://buy.stripe.com/14k8Aa0qn9PV6RO4j1'
         }
       },
 
       GBP: {
         currency: 'GBP',
-        price: 19,
+        price: 8.88,
         paymentLinks: {
-          split: '#NA',
-          full: '#NA'
+          split: 'https://buy.stripe.com/7sIbMm7SP1jp3FCaGR',
+          full: 'https://buy.stripe.com/fZeeYyehd5zFdgc8zi'
         }
       },
 
       PLN: {
         currency: 'PLN',
-        price: 99,
+        price: 39.99,
         paymentLinks: {
-          split: '#NA',
-          full: '#NA'
+          split: 'https://buy.stripe.com/4gwg2C1ur9PV0tqdT6',
+          full: 'https://buy.stripe.com/7sI03Egple6ba406rb'
         }
       },
 
       SGD: {
         currency: 'SGD',
-        price: 28,
+        price: 15.88,
         paymentLinks: {
-          split: '#NA',
-          full: '#NA'
+          split: 'https://buy.stripe.com/aEU03Eb513rxcc8g18',
+          full: 'https://buy.stripe.com/fZeg2Cehd1jpa40g1N'
         }
       },
 
       USD: {
         currency: 'USD',
-        price: 25,
+        price: 11.99,
         paymentLinks: {
-          split: '#NA',
-          full: 'h#NA'
+          split: 'https://buy.stripe.com/9AQ5nYflhd272ByaGT',
+          full: 'https://buy.stripe.com/fZedUu6OL9PV1xubLw'
         }
       }
     }
   },
   */
-  /*{
+  /*{ everything digital - author editon? read it before anyone else? 
     name: 'Author Edition (Everything Digital)',
     badge: 'Read it 2Aug',
     features: ['📲 Everything digital (ePub, PDF, Audiobook)','🤩 Your name printed in the Book Acknowledgements','👩🏻‍🏫 Read it before everyone else','👋 Meet the author via video call'],
@@ -483,7 +483,145 @@ const books: Array<BookOffer> = [
       }
     }
   },*/
-  /*{
+  /*{ This includes the prices of everything digital after 9Aug
+    name: 'eBook only',
+    badge: 'Download now',
+    features: ['📲 ePub, PDF','💾 Download now'],
+    paymentOptions: {
+      AUD: {
+        currency: 'AUD',
+        price: 9.99,
+        paymentLinks: {
+          split: 'https://buy.stripe.com/dR67w6b515zFdgc8z9',
+          full: 'https://buy.stripe.com/3csbMm4GDbY36RO02p'
+        }
+      },
+
+      CAD: {
+        currency: 'CAD',
+        price: 8.99,
+        paymentLinks: {
+          split: 'https://buy.stripe.com/eVa03E2yvaTZ3FC2aM',
+          full: 'https://buy.stripe.com/28o9Ee5KHfafgso4iG'
+        }
+      },
+
+      EUR: {
+        currency: 'EUR',
+        price: 5.99,
+        paymentLinks: {
+          split: 'https://buy.stripe.com/00g5nYdd9faf2BydTv',
+          full: 'https://buy.stripe.com/00g4jU1urbY36RO3eD'
+        }
+      },
+
+      GBP: {
+        currency: 'GBP',
+        price: 4.99,
+        paymentLinks: {
+          split: 'https://buy.stripe.com/9AQdUugplgejgso5n0',
+          full: 'https://buy.stripe.com/dR68Aaehd4vB7VS5mM'
+        }
+      },
+
+      PLN: {
+        currency: 'PLN',
+        price: 25.99,
+        paymentLinks: {
+          split: 'https://buy.stripe.com/5kA17Ib51d271xu4iX',
+          full: 'https://buy.stripe.com/8wM8Aa6OLaTZdgc5mN'
+        }
+      },
+
+      SGD: {
+        currency: 'SGD',
+        price: 8.88,
+        paymentLinks: {
+          split: 'https://buy.stripe.com/6oEaIi3Cz9PV5NK9Dc',
+          full: 'https://buy.stripe.com/cN2aIi0qnaTZekgaH2'
+        }
+      },
+
+      USD: {
+        currency: 'USD',
+        price: 6.99,
+        paymentLinks: {
+          split: 'https://buy.stripe.com/6oE2bMdd92nt5NKg1G',
+          full: 'https://buy.stripe.com/14k17Iflhd27dgc02u'
+        }
+      }
+    }
+  },*/
+  /*{ This includes the prices of everything digital after 9Aug
+    name: 'Audiobook only',
+    badge: 'Download now',
+    features: ['🎤 Audiobook narrated by the author','💾 Download now'],
+    paymentOptions: {
+      AUD: {
+        currency: 'AUD',
+        price: 9.99,
+        paymentLinks: {
+          split: 'https://buy.stripe.com/eVa8Aaflh0flfokg1u',
+          full: 'https://buy.stripe.com/cN203E4GDd274JG5mC'
+        }
+      },
+
+      CAD: {
+        currency: 'CAD',
+        price: 8.99,
+        paymentLinks: {
+          split: 'https://buy.stripe.com/28ocQq5KHgejfok9D7',
+          full: 'https://buy.stripe.com/8wMeYya0XaTZb84aGX'
+        }
+      },
+
+      EUR: {
+        currency: 'EUR',
+        price: 5.99,
+        paymentLinks: {
+          split: 'https://buy.stripe.com/4gw5nY5KH1jpa408z4',
+          full: 'https://buy.stripe.com/14k4jU6OL5zF1xu9CU'
+        }
+      },
+
+      GBP: {
+        currency: 'GBP',
+        price: 4.99,
+        paymentLinks: {
+          split: 'https://buy.stripe.com/6oE3fQ6OL9PV7VS5mT',
+          full: 'https://buy.stripe.com/28odUu6OLe6b7VS02l'
+        }
+      },
+
+      PLN: {
+        currency: 'PLN',
+        price: 25.99,
+        paymentLinks: {
+          split: 'https://buy.stripe.com/bIYg2Cflh5zFcc8dTq',
+          full: 'https://buy.stripe.com/7sIaIia0XbY3ekg3ey'
+        }
+      },
+
+      SGD: {
+        currency: 'SGD',
+        price: 8.88,
+        paymentLinks: {
+          split: 'https://buy.stripe.com/6oEaIi3Cz1jp8ZW5mP',
+          full: 'https://buy.stripe.com/8wM9Ee3Cz1jp2By4ix'
+        }
+      },
+
+      USD: {
+        currency: 'USD',
+        price: 6.99,
+        paymentLinks: {
+          split: 'https://buy.stripe.com/eVag2C8WTaTZ6RO4iR',
+          full: 'https://buy.stripe.com/aEU8Aa0qn2nt7VSaH1'
+        }
+      }
+    }
+  },*/
+  /*{ TEMPLATE
     name: 'eBook only',
     badge: 'Download now',
     features: ['📲 Everything digital (ePub, PDF, Audiobook)','🤩 Your name printed in the Book Acknowledgements','👩🏻‍🏫 Read it before everyone else','👋 Meet the author via video call'],
@@ -552,145 +690,7 @@ const books: Array<BookOffer> = [
       }
     }
   },*/
-  /*{
-    name: 'eBook only',
-    badge: 'Download now',
-    features: ['📲 Everything digital (ePub, PDF, Audiobook)','🤩 Your name printed in the Book Acknowledgements','👩🏻‍🏫 Read it before everyone else','👋 Meet the author via video call'],
-    paymentOptions: {
-      AUD: {
-        currency: 'AUD',
-        price: 9.99,
-        paymentLinks: {
-          split: '#NA',
-          full: '#NA'
-        }
-      },
-
-      CAD: {
-        currency: 'CAD',
-        price: 8.99,
-        paymentLinks: {
-          split: '#NA',
-          full: '#NA'
-        }
-      },
-
-      EUR: {
-        currency: 'EUR',
-        price: 5.99,
-        paymentLinks: {
-          split: '#NA',
-          full: '#NA'
-        }
-      },
-
-      GBP: {
-        currency: 'GBP',
-        price: 4.99,
-        paymentLinks: {
-          split: '#NA',
-          full: '#NA'
-        }
-      },
-
-      PLN: {
-        currency: 'PLN',
-        price: 25.99,
-        paymentLinks: {
-          split: '#NA',
-          full: '#NA'
-        }
-      },
-
-      SGD: {
-        currency: 'SGD',
-        price: 8.88,
-        paymentLinks: {
-          split: '#NA',
-          full: '#NA'
-        }
-      },
-
-      USD: {
-        currency: 'USD',
-        price: 6.99,
-        paymentLinks: {
-          split: '#NA',
-          full: 'h#NA'
-        }
-      }
-    }
-  },*/
-  /*{
-    name: 'eBook only',
-    badge: 'Download now',
-    features: ['📲 Everything digital (ePub, PDF, Audiobook)','🤩 Your name printed in the Book Acknowledgements','👩🏻‍🏫 Read it before everyone else','👋 Meet the author via video call'],
-    paymentOptions: {
-      AUD: {
-        currency: 'AUD',
-        price: 9.99,
-        paymentLinks: {
-          split: '#NA',
-          full: '#NA'
-        }
-      },
-
-      CAD: {
-        currency: 'CAD',
-        price: 8.99,
-        paymentLinks: {
-          split: '#NA',
-          full: '#NA'
-        }
-      },
-
-      EUR: {
-        currency: 'EUR',
-        price: 5.99,
-        paymentLinks: {
-          split: '#NA',
-          full: '#NA'
-        }
-      },
-
-      GBP: {
-        currency: 'GBP',
-        price: 4.99,
-        paymentLinks: {
-          split: '#NA',
-          full: '#NA'
-        }
-      },
-
-      PLN: {
-        currency: 'PLN',
-        price: 25.99,
-        paymentLinks: {
-          split: '#NA',
-          full: '#NA'
-        }
-      },
-
-      SGD: {
-        currency: 'SGD',
-        price: 8.88,
-        paymentLinks: {
-          split: '#NA',
-          full: '#NA'
-        }
-      },
-
-      USD: {
-        currency: 'USD',
-        price: 6.99,
-        paymentLinks: {
-          split: '#NA',
-          full: 'h#NA'
-        }
-      }
-    }
-  },*/
-  /*{
+  /*{ TEMPLATE
     name: 'Audiobook only',
     badge: 'Download now',
     features: ['📲 Everything digital (ePub, PDF, Audiobook)','🤩 Your name printed in the Book Acknowledgements','👩🏻‍🏫 Read it before everyone else','👋 Meet the author via video call'],
