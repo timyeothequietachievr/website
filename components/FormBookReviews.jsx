fe12e7f589bd58f59ce10ed6f97cab27d91a478c
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-07-27 01:02:01 +1000

    form reviews

--- components/FormBookReviews.jsx
+++ components/FormBookReviews.jsx
@@ -6,9 +6,9 @@ export default function SignupBookTellYourFriends() {
   useEffect(() => {
     const script = document.createElement('script');
 
-    script.src = 'https://thequietachievr.ck.page/02951cddec/index.js';
+    script.src = 'https://thequietachievr.ck.page/0a5505f733/index.js';
     script.async = true;
-    script.setAttribute('data-uid', '02951cddec');
+    script.setAttribute('data-uid', '0a5505f733');
     ckFormRef.current.appendChild(script);
 
     return () => {
@@ -19,8 +19,8 @@ export default function SignupBookTellYourFriends() {
     <div className="bg-gray-900 py-16 sm:py-24 lg:py-32">
       <div className="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8">
         <div className="max-w-xl text-3xl font-bold tracking-tight text-white sm:text-4xl lg:col-span-7">
-          <h2 id="tellyourfriends"vclassName="inline sm:block lg:inline xl:block">Tell your friends</h2>{' '}
-          <p className="mx-auto mt-2 max-w-xl text-left text-lg leading-8 text-gray-300">Email a friend who'd be interested in The Quiet Achiever book.</p> 
+          <h2 id="tellyourfriends"vclassName="inline sm:block lg:inline xl:block">Leave a review I can use on my website</h2>{' '}
+          <p className="mx-auto mt-2 max-w-xl text-left text-lg leading-8 text-gray-300"></p> 
           {/* <p className="inline sm:block lg:inline xl:block">It's free.</p> */}
         </div>        
         <form className="w-full max-w-md lg:col-span-5 lg:pt-2">
