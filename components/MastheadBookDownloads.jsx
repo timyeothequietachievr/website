c6ffea0e375f6a728251234f5f6525e77d38dc6c
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-07-27 17:24:14 +1000

    bookdownloads

---
+++ components/MastheadBookDownloads.jsx
@@ -0,0 +1,70 @@
+import ReactPlayer from "react-player/lazy";
+import { useModal, Modal, IntroVideoModalContents } from "./modal";
+
+export default function MastheadBook() {
+  const { open, close, isOpened } = useModal();
+  return (
+    <section className="py-16 bg-white overflow-hidden lg:py-24">
+      <div className="text-center">
+        <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          <span className="block xl:inline">book</span>{' '}
+          {/* <span className="block xl:inline">4-week course for</span>{' '} */}
+          <span className="block text-indigo-600 xl:inline">reviews</span>{' '}
+          {/* <span className="block xl:inline">to have</span>{' '} */}
+          {/* <span className="block text-indigo-600 xl:inline">impact at work</span> */}
+        </h1>
+        <p className="mt-3 max-w-md mx-auto text-base text-gray-800 font-bold sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+        I hope you enjoyed the book. Every review helps!
+        </p>
+        {/*
+        <div className="mt-5 px-4 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+          <div>
+            <a              
+              href="#pricing"
+              className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+              data-event-category="purchase"
+              data-event-action="click_buy_now"
+              data-event-label="cta_book_masthead"
+            >
+              Pre-order Now
+            </a>
+          </div>
+        </div> */}
+        {/*}
+        <div className="mt-5 px-4 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+          <div>
+             <aside ref={ckFormRef} /> 
+          <a              
+              href="https://forms.gle/VAN6n5r8qFhFEsKm7"
+              className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
+              data-event-category="email"
+              data-event-action="click_signup_now"
+              data-event-label="cta_book_masthead"
+            >
+              Get notified
+            </a>      
+          </div> 
+          
+          { /* <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+            <button
+              type="button"
+              onClick={open}
+              className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+              data-event-category="education"
+              data-event-action="click_watch_now"
+              data-event-label="cta_masthead_book"
+            >
+              ▶️ Watch intro
+            </button>
+          </div> 
+        </div>
+        */}
+        {/* <ReactPlayer url="https://www.youtube.com/watch?v=_ZUbIJcvTlM" className="w-full h-auto my-8 mx-auto" controls={true} /> */}
+
+      </div>
+      <Modal open={open} close={close} isOpened={isOpened}>
+        <IntroVideoModalContents ytLink="https://www.youtube.com/embed/_ZUbIJcvTlM?controls=0" />
+      </Modal>
+    </section>
+  )
+}
---
+++ components/TopicsBookDownloads.jsx
@@ -0,0 +1,57 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { VideoCameraIcon, CalendarIcon, UserCircleIcon, ClockIcon } from '@heroicons/react/outline'
+
+const people = [
+  {
+    name: 'Free chapter →',
+    role: '',
+    imageUrl:
+      'src/topic-introducingyourself.png?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    xUrl: '#',
+    linkedinUrl: '#',
+  },
+  {
+    name: 'Checklist',
+    role: '',
+    imageUrl:
+      'src/topic-smalltalk.png?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    xUrl: '#',
+    linkedinUrl: '#',
+  },
+  {
+    name: 'review site 3',
+    role: '',
+    imageUrl:
+      'src/topic-meetings.png?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    xUrl: '#',
+    linkedinUrl: '#',
+  },
+  // More people...
+]
+
+export default function TopicsBook() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">What's in the book?</h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            You can leave a review on: 
+          </p>
+        </div>
+        <ul
+          role="list"
+          className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+        >
+          {people.map((person) => (
+            <li key={person.name}>
+              <img className="aspect-[3/2] w-full rounded-2xl object-cover" src={person.imageUrl} alt="" />
+              <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900"><a href={person.xUrl}>{person.name}</a></h3>
+              <p className="text-base leading-7 text-gray-600">{person.role}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
