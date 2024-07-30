e752d796ab202b8999c968a36db60af7c9186275
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-07-30 23:13:00 +1000

    htt

--- components/QuotesFromTheBook.jsx
+++ components/QuotesFromTheBook.jsx
@@ -17,7 +17,7 @@ const featuredTestimonial = {
             name: 'Chapter: Introduction',
             handle: 'lesliealexander',
             imageUrl:
-              'src/profile-TQABook.jpg',
+              'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
           },
         },
         {
