f7cdb2593141e9f1a819e7c86b06a2a7c626409a
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-08-06 10:08:42 +1000

    sample videos aligned

--- components/TitleBonusCourses.jsx
+++ components/TitleBonusCourses.jsx
@@ -11,7 +11,7 @@ export default function CoursesInclude() {
         <div className="lg:text-center">
           <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase"></h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            But wait, there's more. You also get bonus content on topics not covered in the book, like:
+          You also get bonus content on topics not covered in the book, like:
           </p>
           <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
           {/* 3-4hrs of learning, video lessons, weekly video calls, 1 year access to community. */}
