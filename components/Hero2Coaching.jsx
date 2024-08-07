519996e8eb00ab477922c19ea7f8fda5ef61dc00
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-08-07 22:02:02 +1000

    pricing

--- components/Hero2Coaching.jsx
+++ components/Hero2Coaching.jsx
@@ -9,7 +9,7 @@ export default function HeroCoaching() {
       <div className="relative">
         <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
           <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
-            <div id="bookupdates">
+            <div id="coaching">
               <div className="mt-6">
                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                   1:1 coaching session included
