7119963e7264c26fa38bd3b8c28a6d2dbcec12e6
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-10-15 21:46:46 +1100

    heroicon

--- components/AboutCoaching.jsx
+++ components/AboutCoaching.jsx
@@ -1,4 +1,4 @@
-import { CheckCircleIcon, InformationCircleIcon } from '@heroicons/react/20/solid'
+import { CheckCircleIcon, InformationCircleIcon } from '@heroicons/react/outline'
 
 export default function AboutCoaching() {
   return (
