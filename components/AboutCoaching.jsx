951205ef5e9ca1fe831fd3293b27bdbbabe8955b
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-10-15 19:51:01 +1100

    text component

--- components/AboutCoaching.jsx
+++ components/AboutCoaching.jsx
@@ -1,62 +1,106 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { VideoCameraIcon, CalendarIcon, UserCircleIcon, ClockIcon } from '@heroicons/react/outline'
+import { CheckCircleIcon, InformationCircleIcon } from '@heroicons/react/20/solid'
 
-export default function AboutBook() {
+export default function AboutCoaching() {
   return (
-    <div className="relative bg-white">
-      <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
-        <div className="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6">
-          <div className="mx-auto max-w-2xl lg:mx-0">
-            <h1 className="mt-24 text-4xl font-bold tracking-tight text-gray-900 sm:mt-10 sm:text-6xl">
-              Be coached by someone who is quiet (just like you)
-            </h1>
-            <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            How does it work?
-              </h3>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-            <strong>How does it work?</strong><br></br><br></br>
-            I'm your coach. Over live video calls, we talk through the challenges you are facing and what you want help with right now.<br></br><br></br>
-            Coaching requires chemistry. I can help may people, but I can't help everyone. so we start with a free discovery call to see if we are a good fit.<br></br><br></br>
-            Coaching helps you level up faster. The quiet achievers I can help achieve their goals in days and weeks with my assistance—goals which would normally take them months or years to accomplish on their own.<br></br><br></br>
-            </p>
-            <strong>What can I help you with?</strong><br></br><br></br>
-            <ul role="list">
-                  <li><strong>Quiet leadership:</strong> You are quiet by nature and identify as an introvert; you are not quite sure how to lead when every other leader around you behaves like an extrovert</li>
-                  <li><strong> Get promoted: </strong> You want to be promoted to a leadership role but you feel stuck and don't know what to do to get there</li>
-                  <li><strong>Be more visible:</strong> You do great work but wish you were more seen and visible in your organisation and industry</li>
-                  <li><strong>Public speaking:</strong> You have just said yes to a major public talk/event/podcast, it's coming soon and you don't know what to do next</li>
-                  <li><strong>Interview rehearsal:</strong> You are interviewing for a job you really like and want to show up as the best version of yourself</li>
-                  <li><strong>Interview preparation:</strong> You are interviewing for roles, you've done good work but somehow you never get picked</li>
-                </ul>
-                <strong>Leadership and Team management?</strong><br></br><br></br>
-            <ul role="list">
-                  <li>You are leading/managing a team for the first time and not sure what to do</li>
-                  <li>You have been promoted to a player-coach role but never managed people before</li>
-                  <li>You started as the first designer in your startup/org and suddenly need to hire a team quickly</li>
-                  <li>You are a leader/manager and want an advisor who's walked a similar path before</li>
-                  <li>You are quiet but work with a team with loud voices and you want to learn how to be heard and push back</li>
-                  <li>The team you manage are so quiet and you don't know how to get the best out of them</li>
-                </ul>
-
-            <div className="mt-10 flex items-center gap-x-6">
-              <a
-                href="#pricing"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                See Coaching Options
-              </a>
-              <a href="/about" className="text-sm font-semibold leading-6 text-gray-900">
-                More about Tim <span aria-hidden="true">→</span>
-              </a>
-            </div>
-          </div>
+    <div className="bg-white px-6 py-32 lg:px-8">
+      <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+        <p className="text-base font-semibold leading-7 text-indigo-600">Introducing</p>
+        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">JavaScript for Beginners</h1>
+        <p className="mt-6 text-xl leading-8">
+          Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget
+          aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget. Eleifend
+          egestas fringilla sapien.
+        </p>
+        <div className="mt-10 max-w-2xl">
+          <p>
+            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae
+            sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.
+            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae
+            sed turpis id.
+          </p>
+          <ul role="list" className="mt-8 max-w-xl space-y-8 text-gray-600">
+            <li className="flex gap-x-3">
+              <CheckCircleIcon aria-hidden="true" className="mt-1 h-5 w-5 flex-none text-indigo-600" />
+              <span>
+                <strong className="font-semibold text-gray-900">Data types.</strong> Lorem ipsum, dolor sit amet
+                consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
+                blanditiis ratione.
+              </span>
+            </li>
+            <li className="flex gap-x-3">
+              <CheckCircleIcon aria-hidden="true" className="mt-1 h-5 w-5 flex-none text-indigo-600" />
+              <span>
+                <strong className="font-semibold text-gray-900">Loops.</strong> Anim aute id magna aliqua ad ad non
+                deserunt sunt. Qui irure qui lorem cupidatat commodo.
+              </span>
+            </li>
+            <li className="flex gap-x-3">
+              <CheckCircleIcon aria-hidden="true" className="mt-1 h-5 w-5 flex-none text-indigo-600" />
+              <span>
+                <strong className="font-semibold text-gray-900">Events.</strong> Ac tincidunt sapien vehicula erat
+                auctor pellentesque rhoncus. Et magna sit morbi lobortis.
+              </span>
+            </li>
+          </ul>
+          <p className="mt-8">
+            Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor
+            fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac
+            adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.
+          </p>
+          <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">From beginner to expert in 3 hours</h2>
+          <p className="mt-6">
+            Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh.
+            Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus
+            mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis
+            ipsum eu a sed convallis diam.
+          </p>
+          <figure className="mt-10 border-l border-indigo-600 pl-9">
+            <blockquote className="font-semibold text-gray-900">
+              <p>
+                “Vel ultricies morbi odio facilisi ultrices accumsan donec lacus purus. Lectus nibh ullamcorper ac
+                dictum justo in euismod. Risus aenean ut elit massa. In amet aliquet eget cras. Sem volutpat enim
+                tristique.”
+              </p>
+            </blockquote>
+            <figcaption className="mt-6 flex gap-x-4">
+              <img
+                alt=""
+                src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                className="h-6 w-6 flex-none rounded-full bg-gray-50"
+              />
+              <div className="text-sm leading-6">
+                <strong className="font-semibold text-gray-900">Maria Hill</strong> – Marketing Manager
+              </div>
+            </figcaption>
+          </figure>
+          <p className="mt-10">
+            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae
+            sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.
+          </p>
         </div>
-        <div className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
+        <figure className="mt-16">
           <img
-            className="aspect-[3/2] w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
-            src="src/about-timyeo.jpg?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2102&q=80"
             alt=""
+            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
+            className="aspect-video rounded-xl bg-gray-50 object-cover"
           />
+          <figcaption className="mt-4 flex gap-x-2 text-sm leading-6 text-gray-500">
+            <InformationCircleIcon aria-hidden="true" className="mt-0.5 h-5 w-5 flex-none text-gray-300" />
+            Faucibus commodo massa rhoncus, volutpat.
+          </figcaption>
+        </figure>
+        <div className="mt-16 max-w-2xl">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">Everything you need to get up and running</h2>
+          <p className="mt-6">
+            Purus morbi dignissim senectus mattis adipiscing. Amet, massa quam varius orci dapibus volutpat cras. In
+            amet eu ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut viverra ridiculus non molestie.
+            Gravida quis fringilla amet eget dui tempor dignissim. Facilisis auctor venenatis varius nunc, congue erat
+            ac. Cras fermentum convallis quam.
+          </p>
+          <p className="mt-8">
+            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae
+            sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.
+          </p>
         </div>
       </div>
     </div>
---
+++ components/AboutCoaching2.jsx
@@ -0,0 +1,64 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { VideoCameraIcon, CalendarIcon, UserCircleIcon, ClockIcon } from '@heroicons/react/outline'
+
+export default function AboutBook() {
+  return (
+    <div className="relative bg-white">
+      <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
+        <div className="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h1 className="mt-24 text-4xl font-bold tracking-tight text-gray-900 sm:mt-10 sm:text-6xl">
+              Be coached by someone who is quiet (just like you)
+            </h1>
+            <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            How does it work?
+              </h3>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <strong>How does it work?</strong><br></br><br></br>
+            I'm your coach. Over live video calls, we talk through the challenges you are facing and what you want help with right now.<br></br><br></br>
+            Coaching requires chemistry. I can help may people, but I can't help everyone. so we start with a free discovery call to see if we are a good fit.<br></br><br></br>
+            Coaching helps you level up faster. The quiet achievers I can help achieve their goals in days and weeks with my assistance—goals which would normally take them months or years to accomplish on their own.<br></br><br></br>
+            </p>
+            <strong>What can I help you with?</strong><br></br><br></br>
+            <ul role="list">
+                  <li><strong>Quiet leadership:</strong> You are quiet by nature and identify as an introvert; you are not quite sure how to lead when every other leader around you behaves like an extrovert</li>
+                  <li><strong> Get promoted: </strong> You want to be promoted to a leadership role but you feel stuck and don't know what to do to get there</li>
+                  <li><strong>Be more visible:</strong> You do great work but wish you were more seen and visible in your organisation and industry</li>
+                  <li><strong>Public speaking:</strong> You have just said yes to a major public talk/event/podcast, it's coming soon and you don't know what to do next</li>
+                  <li><strong>Interview rehearsal:</strong> You are interviewing for a job you really like and want to show up as the best version of yourself</li>
+                  <li><strong>Interview preparation:</strong> You are interviewing for roles, you've done good work but somehow you never get picked</li>
+                </ul>
+                <strong>Leadership and Team management?</strong><br></br><br></br>
+            <ul role="list">
+                  <li>You are leading/managing a team for the first time and not sure what to do</li>
+                  <li>You have been promoted to a player-coach role but never managed people before</li>
+                  <li>You started as the first designer in your startup/org and suddenly need to hire a team quickly</li>
+                  <li>You are a leader/manager and want an advisor who's walked a similar path before</li>
+                  <li>You are quiet but work with a team with loud voices and you want to learn how to be heard and push back</li>
+                  <li>The team you manage are so quiet and you don't know how to get the best out of them</li>
+                </ul>
+
+            <div className="mt-10 flex items-center gap-x-6">
+              <a
+                href="#pricing"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                See Coaching Options
+              </a>
+              <a href="/about" className="text-sm font-semibold leading-6 text-gray-900">
+                More about Tim <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
+          <img
+            className="aspect-[3/2] w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
+            src="src/about-timyeo.jpg?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2102&q=80"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
