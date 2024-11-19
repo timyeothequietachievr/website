b87852397523e52af3fbe2daccc9790117ffe2e9
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-11-19 19:45:27 +1100

    30

---
+++ components/Benefits30.jsx
@@ -0,0 +1,49 @@
+import {PricingDisplay} from './pricing-utils/Display'
+
+export default function IntrovertHardBook() {
+    return (
+      <div className="relative bg-white">
+        <div className="lg:absolute lg:inset-0">
+          <div className="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
+            <img
+              /* className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+              src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80" */
+              className="h-56 w-full object-cover lg:absolute lg:h-full"
+              src={"src/TQA-meme-moneypay.svg?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="relative pt-2 pb-16 px-4 sm:pt-2 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-2">
+          <div className="lg:col-start-2 lg:pl-8">
+            <div className="text-base max-w-prose mx-auto lg:max-w-lg lg:ml-auto lg:mr-0">
+              <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                Being a quiet achiever in a loud, noisy world is hard
+              </h3>
+
+
+              <p className="mt-8 text-lg text-gray-500">
+              
+              </p>
+              <div className="mt-5 prose prose-indigo text-gray-500">
+                <ul role="list">
+                  <li>Do people say "you are too quiet" or "you should speak up more", but you don't know how?</li>
+                  <li>Is small talk and networking awkward for you?</li>
+                  <li>In meetings, do you keep quiet even though you have something to say?</li>
+                  <li>Do you avoid public speaking and give away opportunities to present your work? </li>
+                  <li>Do you say "yes" to unreasonable requests even though, in your head, you think "no"?</li>
+                  <li>Do you shy away from difficult conversations but later wish you spoke up?</li>
+                  <li>Do you act like an extrovert at work but you are tired of pretending?</li>
+                  <li>Have read self help books to improve yourself, but all they did was make you feel good but you don't know what to do next?</li>
+                </ul>
+                <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                👉🏻 If your answer is <strong>YES, this book is for you</strong>.
+              </h3>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
---
+++ pages/30habits30days.tsx
@@ -0,0 +1,75 @@
+import ReactPlayer from "react-player/lazy";
+import Pricing from '../components/Pricing'
+import { PricingContextProvider } from '../components/pricing-utils/Context'
+import BeingIntrovertHard from '../components/BeingIntrovertHard'
+import WhatYouWillLearnCourse from '../components/WhatYouWillLearnCourse'
+import WhatIsIncluded from '../components/WhatIsIncluded'
+import CoursesInclude from '../components/CoursesInclude'
+import HowCoursesWork from '../components/HowCoursesWork'
+import CourseInterviews from '../components/CourseInterviews'
+import Feature1 from '../components/Feature1'
+import Feature2 from '../components/Feature2'
+import Feature3 from '../components/Feature3'
+import MonthlyGuestSpeakers from '../components/MonthlyGuestSpeakers'
+import Testimonials from '../components/Testimonials'
+import TestimonialInterview2 from '../components/TestimonialInterview2'
+import TestimonialInterview3 from '../components/TestimonialInterview3'
+import TestimonialInterview4 from '../components/TestimonialInterview4'
+import TestimonialInterview5 from '../components/TestimonialInterview5'
+import TestimonialBook1 from '../components/TestimonialBook1'
+import TestimonialLeadership2 from '../components/TestimonialLeadership2'
+import TestimonialLeadership3 from '../components/TestimonialLeadership3'
+import TestimonialLeadership4 from '../components/TestimonialLeadership4'
+import TestimonialLeadership5 from '../components/TestimonialLeadership5'
+import TestimonialLargeAvatar1 from '../components/TestimonialLargeAvatar1'
+import TestimonialLargeAvatar2 from '../components/TestimonialLargeAvatar2'
+import TestimonialLargeAvatar3 from '../components/TestimonialLargeAvatar3'
+import TestimonialLargeAvatar4 from '../components/TestimonialLargeAvatar4'
+import IntroTQA from '../components/IntroTQA'
+import Cloudlogos from '../components/Cloudlogos'
+import AboutBook from '../components/AboutBook'
+import ConvertkitSide from '../components/ConvertkitSide'
+import ConvertkitCenter from '../components/ConvertkitCenter'
+import MastheadConvertkit from '../components/MastheadConvertkit'
+import HeroFreeChapter from '../components/HeroFreeChapter'
+import NewsletterConvertkit from '../components/NewsletterConvertkit'
+import SignupBookTellYourFriends from '../components/SignupBookTellYourFriends'
+import HeroBookDownloads from '../components/HeroBookDownloads'
+import TopicsGoodBookStores from '../components/TopicsGoodBookStores'
+import Masthead30 from '../components/Masthead30'
+import Hero30 from '../components/Hero30'
+import HowItWorks30 from '../components/HowItWorks30'
+import Benefits30 from '../components/Benefits30'
+import Topics30 from '../components/Topics30'
+import Testimonials30 from '../components/Testimonials30'
+import Reviews30 from '../components/Reviews30'
+
+export default function BookPage() {
+  return (
+    <PricingContextProvider>
+      <Masthead30 />
+      <Hero30 />
+      <HowItWorks30 />
+      <Testimonials30 />
+      <Topics30 />
+      <Reviews30 />
+      <Benefits30 />
+      {/* <FAQ30 /> */}
+      <Pricing kind="bookdigital">
+        <h2 className="text-3xl font-extrabold text-gray-900 sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">
+          eBook and Audiobook
+        </h2>
+        <p className="mt-6 max-w-2xl text-xl text-gray-500">
+        </p>
+      </Pricing>
+      <Pricing kind="bookphysical">
+        <h2 className="text-3xl font-extrabold text-gray-900 sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">
+          Hardcover and Paperback
+        </h2>
+        <p className="mt-6 max-w-2xl text-xl text-gray-500">
+        </p>
+      </Pricing>
+      <SignupBookTellYourFriends />
+    </PricingContextProvider>
+  )
+}
