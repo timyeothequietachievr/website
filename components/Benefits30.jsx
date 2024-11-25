f6e5e645361c604c8d5ee129010596675aef5b84
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2024-11-25 19:04:16 +1100

    return image

--- components/Benefits30.jsx
+++ components/Benefits30.jsx
@@ -4,11 +4,11 @@ export default function IntrovertHardBook() {
     return (
       <div className="relative bg-white">
         <div className="lg:absolute lg:inset-0">
-          <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+          <div className="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
             <img
               /* className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
               src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80" */
-              className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+              className="h-56 w-full object-cover lg:absolute lg:h-full"
               src={"src/30habits-whyjoin.jpg?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"}
               alt=""
             />
--- components/HowItWorks30.jsx
+++ components/HowItWorks30.jsx
@@ -2,21 +2,20 @@ import {PricingDisplay} from './pricing-utils/Display'
 
 export default function IntrovertHardBook() {
     return (
-      <div className="relative bg-white pt-2 pb-32 overflow-hidden">
-        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-          <div className="mt-12 sm:mt-16 lg:mt-0">
-            <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full"> 
-              <img
-                /* className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80" */
-                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none" //change this
-                src={"src/30habits-howitworks.jpg?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"}
-                alt=""
-              />
-            </div>
+      <div className="relative bg-white">
+        <div className="lg:absolute lg:inset-0">
+          <div className="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
+            <img
+              /* className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+              src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80" */
+              className="h-56 w-full object-cover lg:absolute lg:h-full"
+              src={"src/30habits-howitworks.jpg?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"}
+              alt=""
+            />
           </div>
         </div>
-        <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+        <div className="relative pt-2 pb-16 px-4 sm:pt-2 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-2">
+          <div className="lg:col-start-2 lg:pl-8">
             <div className="mt-6">
               <h1 className="mt-2 text-4xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                In our 30 days together:
@@ -43,6 +42,7 @@ export default function IntrovertHardBook() {
                   </a>
               </div>
             </div>
+          </div>
         </div>
       </div>
     )
