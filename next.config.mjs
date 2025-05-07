bfb79fcab3266610babc500d1fd99fa6d49b22dc
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2025-05-07 11:16:45 +1000

    tipjar redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 const amazonBookReviewsUrl = new URL(
   'https://www.amazon.com/Love-You-Like-No-Otter/dp/1728213746/ref=sr_1_1?dib=eyJ2IjoiMSJ9.iM2dGT47yCIAhd358-mJsLg6WbSZrU_Oi-cFjKmouzeuAKE81UK7Rr2C6sziWtsU2JdgSbVh7c2KrSzZSuV3BmM-e-VEE2nFKlcod16tv9Zfza2ydrcLZ5Box6BHWjj4GGHwTEG1Zjtk31jNkXO07bcZGSZHWwoLdhYyrVP8lWfcVw-JvzF72cy2Ouf-Tzt14D_hJ4pNPugimBS_r8usqMNpdFpUcBXRaxfmeacDUaU.YmWF5TZCLsrspj6KN51mNnqGkM5Ls4eASTcfkeJ9yRg&dib_tag=se&keywords=love+you+like+no+otter&qid=1720588521&sr=8-1#reviewsMedley'
+);
+const tipJarUrl = new URL(
+  'https://thequietachievr.kit.com/products/tip-jar'
 )
+
 
 const nextConfig = {
   async redirects() {
@@ -8,6 +12,11 @@ const nextConfig = {
       {
         source: '/bookreview',
         destination: amazonBookReviewsUrl.toString(),
+        permanent: true,
+      },
+      {
+        source: '/tipjar',
+        destination: tipJarUrl.toString(),
         permanent: true,
       },
     ]
