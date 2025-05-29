93660ad4a9f3bc47b89ca7cf4b08a821c53f6ea6
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2025-05-29 23:39:20 +1000

    coaching page masthead

--- components/MastheadCoaching.jsx
+++ components/MastheadCoaching.jsx
@@ -6,14 +6,17 @@ export default function MastheadCoaching() {
     <section className="py-16 bg-white overflow-hidden lg:py-24">
       <div className="text-center">
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-          <span className="block xl:inline">Coaching for</span>{' '}
-          {/* <span className="block xl:inline">4-week course for</span>{' '} */}
+          <span className="block xl:inline">Coach for quiet achievers</span>{' '}
+          {/* <span className="block xl:inline">4-week course for</span>{' '} 
           <span className="block text-indigo-600 xl:inline">quiet achievers</span>{' '}
           <span className="block xl:inline">to have</span>{' '}
-          <span className="block text-indigo-600 xl:inline">impact at work</span>
+          <span className="block text-indigo-600 xl:inline">impact at work</span> */}
         </h1>
         <p className="mt-3 max-w-md mx-auto text-base text-gray-800 font-bold sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-        without pretending to be extroverts.
+        who want to be leaders, be seen, be heard
+        </p>
+        <p className="mt-3 max-w-md mx-auto text-base text-gray-800 font-bold sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+        but work in places with strong opinions and loud voices.
         </p>
         {/*}
         <div className="mt-5 px-4 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
