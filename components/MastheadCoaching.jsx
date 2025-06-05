b7560e1073a629ce1a59ddfa3ef01e265fa57121
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2025-06-06 08:49:23 +1000

    cta

--- components/MastheadCoaching.jsx
+++ components/MastheadCoaching.jsx
@@ -21,7 +21,7 @@ export default function MastheadCoaching() {
         <div className="mt-5 px-4 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
           <div className="rounded-md shadow">
             <a
-              href="#pricing"
+              href="#bookacall"
               className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
               data-event-category="purchase"
               data-event-action="click_coaching_call"
