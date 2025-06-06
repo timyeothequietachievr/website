df1d6c4a284361c438162b820288a255bdb1f438
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2025-06-06 19:05:06 +1000

    emailoptout

---
+++ pages/emailoptout.tsx
@@ -0,0 +1,8 @@
+import ReactPlayer from "react-player/lazy";
+import MastheadEmailOptOut from '../components/MastheadEmailOptOut'
+
+export default function EmailOptOutPage() {
+  return (
+      <MastheadEmailOptOut />
+  )
+}
