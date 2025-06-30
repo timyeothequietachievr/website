15ce13d6052f2787697288462e7982e24cb9449b
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2025-06-30 22:36:39 +1000

    emailoptin

---
+++ pages/emailoptin.tsx
@@ -0,0 +1,8 @@
+import ReactPlayer from "react-player/lazy";
+import MastheadEmailOptIn from '../components/MastheadEmailOptIn'
+
+export default function EmailOptInPage() {
+  return (
+      <MastheadEmailOptIn />
+  )
+}
