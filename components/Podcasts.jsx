1250e34fc2a3ec0f18deae170738627cfb879578
timyeothequietachievr <89186480+timyeothequietachievr@users.noreply.github.com>
2025-09-04 11:51:28 +1000

    jpg

--- components/Podcasts.jsx
+++ components/Podcasts.jsx
@@ -38,7 +38,7 @@ export default function Podcasts() {
               <img
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
                 /* src="https://www.youtube.com/embed/QRn5SFM97pA" */
-                src="src/podcast-quietstrong.png"
+                src="src/podcast-quietstrong.jpg"
                 alt="Quiet and Strong Podcast"
               />
             </div>
